48ecd2eb8f854d1a3e337c8f229802004d3b6924
Charles B. <charlesbanks238@gmail.com>
2024-08-08 05:06:10 -0700

    Adds the begining of the functionality to book a new room and adds additional tests for the searching the rooms by room type.

--- src/api-calls.js
+++ src/api-calls.js
@@ -24,47 +24,7 @@ export async function fetchCustomer(num) {
   };
   // console.log(verifiedCustomer)
   return verifiedCustomer;
-
-  
-  // let customerExists = gotCustomers.customers.find((customer) => {
-  //   console.log('conditional in fetchCustomer: ', customer.id == num, num, customer.id)
-  //   customer.id === num;
-  //   if (customer.id === num) {
-  //     getCustomer(customer);
-  //     return true;
-  //   } else if (!customer.id === num) {
-  //     return console.log('That user doesn\'t exist!');
-  //   };
-  // });
-
-  // function checkCustomers(custNum) {
-  //   console.log(custNum.id == num, custNum.id, num)
-  //   return custNum.id == num;
-  // };
-
-  // let customerExists = gotCustomers.customers.find(checkCustomers);
-
-  // let customerExists = gotCustomers.customers.reduce((acc, customer) => {
-  //   // console.log('HERE: ', customer.id == num, customer.id, num)
-  //   if (customer.id == num) {
-  //     getCustomer(customer);
-  //     acc = customer;
-  //   };
-  //   return acc;
-  // }, {});
-
-  // console.log('find in fetch customer func: ', customerExists.id === undefined)
-
-  // let checkedCustomer = () => {
-  //   if (customerExists.id === undefined) {
-  //     console.log('Customer doesn\'t exist!!!')
-  //   } else if (!customerExists.id === undefined) {
-  //     return true;
-  //   }
-  // };
-}
-
-// fetchCustomer(27);
+};
 
 export async function fetchBookings(someData) {
   let parsedCustomerBookings = []
@@ -95,3 +55,19 @@ export async function fetchRooms() {
 
   getRoomReference(roomsFetched.rooms);
 };
+
+export async function postNewBooking(anID, aDateString, aRoomNum) {
+  const fetchToPost = await fetch("http://localhost:3001/api/v1/bookings",{
+    method: 'POST',
+    body: JSON.stringify(
+      { "userID": anID, "date": aDateString, "roomNumber": aRoomNum }
+    ),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const postToFetch = await fetchToPost.json()
+  console.log(postToFetch)
+};
+
+// postNewBooking();
